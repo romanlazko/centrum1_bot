dfcb5f93cf0f4cdfcb26b43b5ad1d5f29db372fd
romanlazko <romanlazko@gmail.com>
2024-04-28 15:24:23 +0200

    Исправление багов

--- resources/views/components/message/buttons.blade.php
+++ resources/views/components/message/buttons.blade.php
@@ -3,7 +3,7 @@
         @foreach ($vertical_lines as $buttons)
             <div class="grid grid-cols-{{ count($buttons) }} text-center" >
                 @foreach ($buttons as $button)
-                    <div class="flex flex-col shadow sm:rounded-lg p-2 m-1 bg-gray-200" title="{{ $button->callback_data }}">
+                    <div class="flex flex-col shadow sm:rounded-lg p-2 m-1 bg-gray-200">
                         {{ $button->text }}
                     </div>
                 @endforeach
